658ac58e16824141befd2a3a6674082f5aeccc2d
Birkan <birkanpusa@icloud.com>
2021-12-21 19:23:36 +0300

    yorumladım

--- FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import LocalAuthentication //kimlik doğrulama için import
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //butona tıklandığı zaman ne olacak
+    @IBAction func loginButtonClicked(_ sender: Any) {
+        
+        let authContext = LAContext() //
+        
+        var error : NSError? //fonksiyona error tanımlamak için error tipinde obje oluşturduk
+        
+        
+        
+        //parmakizi veya yüze göre doğrulama
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            //biometrik veri kontrolü yaptırdık
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you ?") { success, error in
+                //eğer faceid eşleşiyorsa
+                if success == true {
+                    //oldu
+                    DispatchQueue.main.async { //main threadda çalıştırdık
+                    self.performSegue(withIdentifier: "toSecondVC", sender: nil) //butona tıklayınca tosecondVC'ye gönderdik
+                        self.myLabel.text = "Login succesfull"
+                    }
+                }else {
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "error"
+                    }
+                    
+                }
+            }
+        }
+            
+    }
+    
 }
 
